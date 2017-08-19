e4c6c6fd0c650ea149c12b20bfd4b902e16fbdd9
Jonathan <carusyte@163.com>
2017-08-19 13:16:06 +0800

    test rpc

---
+++ rima/rpcclient_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"testing"
+	"net/rpc"
+	"log"
+	"fmt"
+)
+
+const serverAddress = "115.159.237.46:45321"
+
+func TestClient(t *testing.T) {
+	client, err := rpc.DialHTTP("tcp", serverAddress)
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+
+	// Synchronous call
+	args := Args{7, 8}
+	var reply int
+	err = client.Call("Arith.Multiply", args, &reply)
+	if err != nil {
+		log.Fatal("arith error:", err)
+	}
+	fmt.Printf("Arith: %d*%d=%d", args.A, args.B, reply)
+
+	// Asynchronous call
+	quotient := new(Quotient)
+	divCall := client.Go("Arith.Divide", args, quotient, nil)
+	replyCall := <-divCall.Done // will be equal to divCall
+	// check errors, print, etc.
+	if replyCall.Error != nil {
+		log.Fatal("arith error:", err)
+	}
+	q := replyCall.Reply.(Quotient)
+	fmt.Printf("Arith: %d/%d=%d, %d", args.A, args.B, q.Quo, q.Rem)
+}
