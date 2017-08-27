b0251a36e19809a14ae008f9bcdd8bd1a2f13403
Jonathan <carusyte@163.com>
2017-08-27 20:50:02 +0800

    modify wdc to read /etc/yum.repos.d/CentOS-Base.repo

--- wdc/wdc.go
+++ wdc/wdc.go
@@ -23,7 +23,7 @@ func main() {
 	gio.Init()   // If the command line invokes the mapper or reducer, execute it and exit.
 	//flag.Parse() // optional, since gio.Init() will call this also.
 	f := flow.New("top5 words in yum").
-		Read(file.Txt("/etc/yum.repos.d/CentOS-Vault.repo", 2)). // read a txt file and partitioned to 2 shards
+		Read(file.Txt("/etc/yum.repos.d/CentOS-Base.repo", 2)). // read a txt file and partitioned to 2 shards
 		Map("tokenize", mapper.Tokenize). // invoke the registered "tokenize" mapper function.
 		Map("addOne", mapper.AppendOne).  // invoke the registered "addOne" mapper function.
 		ReduceByKey("sum", reducer.Sum). // invoke the registered "sum" reducer function.
