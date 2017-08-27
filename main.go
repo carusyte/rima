79d5d578ea9af2a0347573e157978d0bed217ade
Jonathan <carusyte@163.com>
2017-08-27 23:45:46 +0800

    change rpc http server to tcp server

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/chrislusf/gleam/gio"
 	"net"
-	"net/http"
 	"net/rpc"
 	"log"
 	"github.com/carusyte/rima/rsec"
@@ -13,16 +12,19 @@ import (
 func main() {
 	gio.Init() // If the command line invokes the mapper or reducer, execute it and exit.
 
+	svr := rpc.NewServer()
 	//Register RPC services
-	rpc.Register(new(rsec.IndcScorer))
-	rpc.Register(new(rsec.DataSync))
-	rpc.Register(new(tes.GTest))
+	svr.Register(new(rsec.IndcScorer))
+	svr.Register(new(rsec.DataSync))
+	svr.Register(new(tes.GTest))
 
-	rpc.HandleHTTP()
 	l, e := net.Listen("tcp", ":45321")
 	if e != nil {
 		log.Fatal("listen error:", e)
 	}
 	log.Printf("listening on port: %d", 45321)
-	http.Serve(l, nil)
+
+	// This statement links rpc server to the socket, and allows rpc server to accept
+	// rpc request coming from that socket.
+	svr.Accept(l)
 }
