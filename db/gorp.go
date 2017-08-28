cf89342b3377554a6f9bc2c616418fabcc2f2622
Jonathan <carusyte@163.com>
2017-08-28 11:19:07 +0800

    de oracle

--- db/gorp.go
+++ db/gorp.go
@@ -4,7 +4,8 @@ import (
 	"gopkg.in/gorp.v2"
 	"github.com/carusyte/stock/util"
 	"database/sql"
-	_ "gopkg.in/rana/ora.v4"
+	//_ "gopkg.in/rana/ora.v4"
+	"github.com/carusyte/stock/db"
 )
 
 var (
@@ -20,17 +21,17 @@ func Ora() *gorp.DbMap {
 }
 
 func initGorp() {
-	db, err := sql.Open("ora", "rima/rima@10.16.53.30:1521/hundsun")
-	util.CheckErr(err, "sql.Open failed,")
-
-	db.SetMaxOpenConns(64)
-	db.SetMaxIdleConns(64)
-
-	// construct a gorp DbMap
-	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.OracleDialect{}}
-
-	util.CheckErr(db.Ping(), "Failed to ping db,")
-
-	ora = dbmap
-	inited = true
+	//db, err := sql.Open("ora", "rima/rima@10.16.53.30:1521/hundsun")
+	//util.CheckErr(err, "sql.Open failed,")
+	//
+	//db.SetMaxOpenConns(64)
+	//db.SetMaxIdleConns(64)
+	//
+	//// construct a gorp DbMap
+	//dbmap := &gorp.DbMap{Db: db, Dialect: gorp.OracleDialect{}}
+	//
+	//util.CheckErr(db.Ping(), "Failed to ping db,")
+	//
+	//ora = dbmap
+	//inited = true
 }
