e6614907f8f204efef7d758e8f9ff7a31a809f53
Jonathan <carusyte@163.com>
2017-08-31 10:16:18 +0800

    modify viper usage

--- conf/args.go
+++ conf/args.go
@@ -5,21 +5,28 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var Args ArgsStruct
+var Args Arguments
 
-type ArgsStruct struct {
-	CouchbaseServers string
+type Arguments struct {
+	CouchbaseServers string `mapstructure:"couchbase_servers"`
 }
 
 func init() {
-	viper.SetConfigName("rima") // name of config file (without extension)
-	viper.AddConfigPath(".")      // optionally look for config in the working directory
-	viper.AddConfigPath("$GOPATH/bin")
-	viper.AddConfigPath("$HOME")
-	err := viper.Unmarshal(&Args) // Find and read the config file
+	v := viper.New()
+	v.SetConfigName("rima") // name of config file (without extension)
+	v.AddConfigPath(".")      // optionally look for config in the working directory
+	v.AddConfigPath("$GOPATH/bin")
+	v.AddConfigPath("$HOME")
+	err := v.ReadInConfig()
+	if err != nil {
+		logrus.Errorf("config file error: %+v", err)
+		return
+	}
+	err = v.Unmarshal(&Args) // Find and read the config file
 	if err != nil {
 		logrus.Printf("config file error: %s", err)
 	} else {
-		viper.WatchConfig()
+		logrus.Printf("Configuration: %+v", Args)
+		v.WatchConfig()
 	}
 }
