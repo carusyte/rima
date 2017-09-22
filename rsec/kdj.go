5eaeb5288b20b40ada7e721e7702e1c9b953bf89
Jonathan <carusyte@163.com>
2017-09-22 17:26:38 +0800

    fix kv key inconsistency

--- rsec/kdj.go
+++ rsec/kdj.go
@@ -427,17 +427,7 @@ func kdjPruneMapper(row []interface{}) (e error) {
 	prec := gio.ToFloat64(m["Prec"])
 	refIdx := int(gio.ToInt64(m["RefIdx"]))
 	var fdvs []*model.KDJfdView
-	for i := 0; i < 3; i++ {
-		cache.Cb().Get(kdjFdRawKey(id), &fdvs)
-		if len(fdvs) > 0 {
-			break
-		}
-		logr.Warnf("[%s] no data in cache server. refIdx:%d, len:%d", id, refIdx, len(fdvs))
-		time.Sleep(time.Second * 2)
-	}
-	if len(fdvs) == 0 {
-		return errors.Errorf("[%s] no data in cache server. refIdx:%d", id, refIdx)
-	}
+	cache.Cb().Get(id, &fdvs)
 	kdjs := fdvs[refIdx:]
 	logr.Debugf("kdjPruneMapper KDJs size: %d", len(kdjs))
 	f1 := kdjs[0]
